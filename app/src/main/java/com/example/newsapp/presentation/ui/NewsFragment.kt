82a6015b544b0234834cc1f0ff76069f040a92f3
altunoymak <yusuf_altunoymak@hotmail.com>
2024-05-01 15:56:22 +0300

    Base Structure and Gradle Done

--- app/src/main/java/com/example/newsapp/presentation/ui/NewsFragment.kt
+++ app/src/main/java/com/example/newsapp/presentation/ui/NewsFragment.kt
@@ -1,11 +1,7 @@
 package com.example.newsapp.presentation.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.example.newsapp.R
 import com.example.newsapp.base.BaseFragment
 import com.example.newsapp.databinding.FragmentNewsBinding
 
