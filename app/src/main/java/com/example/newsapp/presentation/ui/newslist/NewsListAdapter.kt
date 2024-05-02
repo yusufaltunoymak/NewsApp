44bcc12019635e9d38301aa52b141ad61fa46a84
altunoymak <yusuf_altunoymak@hotmail.com>
2024-05-02 13:47:48 +0300

    Implements Text Trimming and Regex Clean Up

--- app/src/main/java/com/example/newsapp/presentation/ui/newslist/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsapp/presentation/ui/newslist/NewsListAdapter.kt
@@ -1,28 +1,34 @@
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.newsapp.data.model.Article
 import com.example.newsapp.databinding.ListRecyclerItemBinding
+import com.example.newsapp.util.cleanText
+import com.example.newsapp.util.downloadFromUrl
 
 class NewsListAdapter : ListAdapter<Article, NewsListAdapter.ViewHolder>(NewsListCallBack()) {
-    inner class ViewHolder(private val binding: ListRecyclerItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: ListRecyclerItemBinding, val context : Context) : RecyclerView.ViewHolder(binding.root) {
         fun bind(article: Article) {
             binding.apply {
                 if (!article.urlToImage.isNullOrEmpty()) {
+                    newsPosterImage.downloadFromUrl(article.urlToImage, context)
                     Glide.with(itemView.context).load(article.urlToImage).into(newsPosterImage)
                 }
                 newsHeaderText.text = article.title
-                newsContentText.text = article.content
+                newsContentText.text = cleanText(article.content)
             }
         }
     }
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ListRecyclerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+        return ViewHolder(binding,parent.context)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
---
+++ app/src/main/java/com/example/newsapp/util/Extensions.kt
@@ -0,0 +1,23 @@
+package com.example.newsapp.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.newsapp.R
+
+fun ImageView.downloadFromUrl(url: String?, context: Context) {
+    val options = RequestOptions()
+        .error(ContextCompat.getDrawable(context, R.color.light_grey))
+
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(url)
+        .dontAnimate()
+        .fitCenter().into(this)
+}
+
+fun cleanText(text: String?): String {
+    return text?.replace(Regex("<[^>]*>|&[^;]+;|\\r\\n|\\t"), "")?.replace(Regex("[^A-Za-z0-9\\s]+"), "") ?: ""
+}
