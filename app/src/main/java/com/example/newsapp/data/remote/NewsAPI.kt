51f5b92370c0ab327e6eb28a484a211bfacb7a9f
altunoymak <yusuf_altunoymak@hotmail.com>
2024-05-04 18:53:45 +0300

    Code Formatter

--- app/src/main/java/com/example/newsapp/data/remote/NewsAPI.kt
+++ app/src/main/java/com/example/newsapp/data/remote/NewsAPI.kt
@@ -2,16 +2,15 @@ package com.example.newsapp.data.remote
 
 import com.example.newsapp.data.model.NewsResponse
 import com.example.newsapp.util.Constants
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsAPI {
     @GET("everything")
     suspend fun getNewsList(
-        @Query("q") query : String,
-        @Query("page") page : Int,
-        @Query("apiKey") apiKey : String = Constants.API_KEY,
-        @Query("pageSize") pageSize : Int
-    ) : NewsResponse
+        @Query("q") query: String,
+        @Query("page") page: Int,
+        @Query("apiKey") apiKey: String = Constants.API_KEY,
+        @Query("pageSize") pageSize: Int
+    ): NewsResponse
 }
--- app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/domain/repository/NewsRepository.kt
@@ -2,10 +2,8 @@ package com.example.newsapp.domain.repository
 
 import androidx.paging.PagingData
 import com.example.newsapp.data.model.Article
-import com.example.newsapp.data.model.NewsResponse
-import com.example.newsapp.data.response.Response
 import kotlinx.coroutines.flow.Flow
 
-    interface NewsRepository {
-        suspend fun getNewsList() :Flow<PagingData<Article>>
-    }
+interface NewsRepository {
+    suspend fun getNewsList(): Flow<PagingData<Article>>
+}
