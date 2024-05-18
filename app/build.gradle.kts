4f1915753496746236d3fac236ac3b3ebff1951d
altunoymak <yusuf_altunoymak@hotmail.com>
2024-05-18 16:29:16 +0300

    Version Catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,6 @@ android {
 }
 
 dependencies {
-    val paging_version = "3.2.1"
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -62,51 +61,38 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
     //Hilt
-    implementation ("com.google.dagger:hilt-android:2.48.1")
-    kapt("com.google.dagger:hilt-compiler:2.48.1")
-
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
     //Navigation
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.7.6")
-
+    implementation (libs.androidx.navigation.fragment.ktx)
+    implementation (libs.androidx.navigation.ui.ktx)
     //Retrofit
-    implementation ("com.google.code.gson:gson:2.9.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-
+    implementation (libs.gson)
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
     //Glide
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-
+    implementation (libs.glide)
     //Logging Interceptor
-    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
-
+    implementation (libs.okhttp)
+    implementation (libs.logging.interceptor)
     //Room
-    implementation ("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-    annotationProcessor ("androidx.room:room-compiler:2.6.1")
-    implementation ("androidx.room:room-ktx:2.6.1")
-
+    implementation (libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    annotationProcessor (libs.androidx.room.compiler)
+    implementation (libs.androidx.room.ktx)
     //Lottie
-    implementation("com.airbnb.android:lottie:5.2.0")
-
+    implementation(libs.lottie)
     //Paging
-    implementation("androidx.paging:paging-runtime:$paging_version")
-
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation ("androidx.lifecycle:lifecycle-common-java8:2.6.1")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.1")
-
+    implementation(libs.androidx.paging.runtime)
+    //ViewModel
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.runtime.ktx)
+    implementation (libs.androidx.lifecycle.livedata.ktx)
+    implementation (libs.androidx.lifecycle.common.java8)
+    implementation (libs.androidx.lifecycle.viewmodel.savedstate)
     //Gemini
-    implementation("com.google.ai.client.generativeai:generativeai:0.1.2")
-
-
-
-
+    implementation(libs.generativeai)
 }
 kapt {
     correctErrorTypes = true
